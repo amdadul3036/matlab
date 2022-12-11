4632235464b11ae471d07085cc1658b18d885c4c
MD AMDADUL ISLAM <amdadul3036@gmail.com>
2022-12-11 22:17:29 +0600

    Learned 3 MATLAB Commands today

---
+++ commands.m
@@ -0,0 +1,30 @@
+disp("Hello Dhrubo")
+Hello Dhrubo
+load('commands.mat')
+Error using load
+Unable to read MAT-file C:\Users\User\OneDrive\Documents\MATLAB\MATLAB Learning\commands.mat. Not a binary
+MAT-file. Try load -ASCII to read as text.
+ 
+a=3;
+disp(a);
+     3
+
+fprintf("Hello Goobly");
+Hello Goobly>> fprintf("Hello Goobly"/n);
+Unrecognized function or variable 'n'.
+ 
+
+fprintf("Hello Goobly/n");
+Hello Goobly/n>> fprintf("Hello Goobly\n");
+Hello Goobly
+a=5;
+b=6;
+c=a+b;
+fprintf("The Sum of %d and %d is %d",a,b,c );
+The Sum of 5 and 6 is 11>> fprintf("The Sum of %d and %d is %d",a,b,c );
+The Sum of 5 and 6 is 11>> fprintf("The Sum of %d and %d is %d\n",a,b,c );
+The Sum of 5 and 6 is 11
+a = input('');
+5
+a = input('');
+8
